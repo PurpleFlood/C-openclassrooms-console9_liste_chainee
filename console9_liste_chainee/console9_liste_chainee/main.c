b72842946ac67861ca3abeffe0298738f10abe2f
purple_flood <49845374+PurpleFlood@users.noreply.github.com>
2024-04-16 10:27:46 +0200

    Create main.c

---
+++ console9_liste_chainee/console9_liste_chainee/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  console9_liste_chainee
+//
+//  Created by asap on 16/04/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
